15b1aa301b5a7760c17248bc646a0a9f3b9e00b0
Johnson Tseng <vtjc2002@gmail.com>
2023-11-16 14:02:39 -0500

    add tests for parquet

--- src/SqlUnitTestDemo/End2EndTests/RowCountByYearQuarterTests.cs
+++ src/SqlUnitTestDemo/End2EndTests/RowCountByYearQuarterTests.cs
@@ -1,6 +1,10 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
 using SqlUnitTestDemo.Models;
+using System;
+using System.Formats.Asn1;
+using System.Text.RegularExpressions;
 
 namespace SqlUnitTestDemo.End2EndTests
 {
@@ -10,7 +14,10 @@ namespace SqlUnitTestDemo.End2EndTests
 
         public RowCountByYearQuarterTests(End2EndTestFixture fixture) => _fixture = fixture;
 
-        
+        /// <summary>
+        /// Example of querying sql db as source and sql dw as target and comparing the results
+        /// </summary>
+        /// <returns></returns>
         [Fact(DisplayName ="End to EndTest Sql year and quarter count equals Sql Dw data")]
         public async Task RowCountByYearShouldMatch()
         {
@@ -26,6 +33,8 @@ namespace SqlUnitTestDemo.End2EndTests
             var targetdata = await sqldwConnection.QueryAsync<YearQuarterCount>(sqldwquery);
             
             //Assert
+
+            //The count of rows should be the same
             Assert.Equal(sourcedata.Count(), targetdata.Count());
 
             //Loop through each row and compare the row count
@@ -36,22 +45,75 @@ namespace SqlUnitTestDemo.End2EndTests
             }
         }
 
+        /// <summary>
+        /// Example of querying ADLSv2 delta format using Synapse Sql Serverless.
+        /// </summary>
+        /// <returns></returns>
         [Fact(DisplayName = "The distinct count of years should match from the delta file")]
-        public async Task DistinctCountOfYearOfDeltaFormatShouldMatch()
+        public async Task DeltaFormatDistinctCountOfYearOfShouldMatch()
         {
             //Setup
             var synapseServerlessConnection = new SqlConnection(await _fixture.GetSynapseSqlServerLessTargetConnectionString());
+            var sourceData = new List<Year> { new Year { year = 2019 }, new Year { year = 2020 } };
 
             //This dataset is publicly available in Azure learn https://learn.microsoft.com/en-us/azure/synapse-analytics/sql/query-delta-lake-format
-            var sqlquery = "SELECT distinct([year]) FROM OPENROWSET( BULK 'https://sqlondemandstorage.blob.core.windows.net/delta-lake/covid/', FORMAT = 'delta') as rows ORDER By [year];";
+            var sqlquery = "SELECT DISTINCT([year]) ";
+            sqlquery += "FROM OPENROWSET( BULK 'https://sqlondemandstorage.blob.core.windows.net/delta-lake/covid/', FORMAT = 'delta') ";
+            sqlquery += "WITH ([year] smallint) ";
+            sqlquery += "AS rows ORDER BY [year];";
 
             //Act
-            var targetdata = await synapseServerlessConnection.QueryAsync<Year>(sqlquery);
-            var sortedTargetdata = targetdata.OrderBy(x => x.year);
+            var targetData = await synapseServerlessConnection.QueryAsync<Year>(sqlquery);
 
             //Assert
-            Assert.True(sortedTargetdata.First().year == 2019, $"The first year should be 2019 but it's {sortedTargetdata.First().year}.");
-            Assert.True(sortedTargetdata.Last().year == 2020, $"The last year should be 2020 but it's {sortedTargetdata.Last().year}.");
+
+            //the count should be the same
+            Assert.True(sourceData.Count() == targetData.Count(), $"The count of years should be the same but target distinct count is {targetData.Count()}.");
+
+            //the years should be the same
+            sourceData.ForEach( source =>
+            {
+                var compare = targetData.Where(x => x.year == source.year).First();
+                Assert.True(source.year == compare.year, $"The source year {source.year} should be the same but target year is {compare.year}.");
+            });
+        }
+
+        /// <summary>
+        /// Example of querying ADLSv2 parquet format using Synapse Sql Serverless.
+        /// </summary>
+        [Fact(DisplayName = "The sum of death by year should match from the parquet file")]
+        public async Task ParquetFormatDeathSumShouldMatch()
+        {
+            // Setup
+            var synapseServerlessConnection = new SqlConnection(await _fixture.GetSynapseSqlServerLessTargetConnectionString());
+
+            // Intentionally using month = 0 to indicate sum.
+            var sourceData = new List<CovidDeath>() { new CovidDeath { Year = 2019, Month = 0, Deaths = 0 }, new CovidDeath { Year = 2020, Month = 0, Deaths = 1612833 } };
+
+            //This dataset is publicly available in Azure learn https://learn.microsoft.com/en-us/azure/synapse-analytics/sql/query-parquet-files
+            var sqlquery = "SELECT [year], month, sum(deaths) as deaths " +
+            "FROM openrowset( BULK 'https://pandemicdatalake.blob.core.windows.net/public/curated/covid-19/ecdc_cases/latest/ecdc_cases.parquet', " +
+            "FORMAT = 'parquet') " +
+            "WITH([year] smallint, [month] TINYINT, deaths int) " +
+            "AS rows " +
+            "GROUP BY[year], [month];";
+
+            // Act
+            var targetData = await synapseServerlessConnection.QueryAsync<CovidDeath>(sqlquery);
+
+            // here we are grouping the data by year and month and summing the death count. Intentionally using month = 0 to indicate sum.
+            var targetDataGrouped = targetData.GroupBy(x => new { x.Year }).Select(x => new CovidDeath { Year = x.Key.Year, Deaths = x.Sum(y => y.Deaths) });
+
+
+            // Assert
+            Assert.NotEmpty(targetData);
+
+            sourceData.ForEach(source =>
+            {
+                var compare = targetDataGrouped.Where(x => x.Year == source.Year && x.Month == 0).First();
+                Assert.True(source.Deaths == compare.Deaths, $"The source death count {source.Deaths} should be the same but target death count is {compare.Deaths}.");
+            });
+
         }
 
     }
---
+++ src/SqlUnitTestDemo/Models/CovidDeath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUnitTestDemo.Models
+{
+    public class CovidDeath
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Deaths { get; set; }
+    }
+}
